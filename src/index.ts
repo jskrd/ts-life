6a91a1c210894c04eb3c8a635e9f419c0da516ce
Joe S. <joe@skrd.io>
2024-04-08 23:59:55 +0100

    refactor: move game's initialization to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,3 @@
-import { Generation } from "./models/Generation";
-import { Universe } from "./models/Universe";
-import { resizeCanvasToViewport } from "./utils";
+import { play } from "./game";
 
-const canvas = document.getElementById("app");
-if (!(canvas instanceof HTMLCanvasElement)) {
-	throw new Error("Canvas not found");
-}
-
-const scale = 4;
-
-resizeCanvasToViewport(canvas, scale);
-
-const generation = Generation.seed(canvas.width / scale, canvas.height / scale);
-const universe = new Universe(generation);
-
-const context = canvas.getContext("2d");
-if (!(context instanceof CanvasRenderingContext2D)) {
-	throw new Error("Canvas context not found");
-}
-
-const tick = () => {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.drawImage(universe.toCanvas(canvas.width, canvas.height), 0, 0);
-
-	universe.tick();
-};
-
-setInterval(tick, 100);
+play();
