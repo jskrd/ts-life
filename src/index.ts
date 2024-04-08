f6dd98e5c5a2f280462e88f24cf67868b2177fad
Joe S. <joe@skrd.io>
2024-04-09 00:04:18 +0100

    feat: install Vercel Speed Insights to collect performance metrics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { play } from "./game";
 
 inject();
+injectSpeedInsights();
+
 play();
